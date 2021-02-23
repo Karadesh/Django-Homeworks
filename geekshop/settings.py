a5c5859d3576f5c7c71e7aa9f83304282c49370a
Karadesh <62292688+Karadesh@users.noreply.github.com>
2021-02-23 14:41:13 +0300

    Lesson 6 (#7)
    * image bug fixes. Dat pillow drives me mad
    
    * Lesson 6. Basket extension
    
    * Lesson 6. Hot product
    
    * Lesson 6. Basket read and delete
    
    * Lesson 6. Product page
    
    * Lesson 6. Login fix
    
    * Lesson 6. After login
    
    * Lesson 6. AJAX

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -31,15 +31,15 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'mainapp',
-    'authnapp',
-    'basketapp',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "mainapp",
+    "authnapp",
+    "basketapp",
 ]
 
 # Auth model
@@ -131,3 +131,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 MEDIA_URL = "/media/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+# Set login path:
+#   https://docs.djangoproject.com/en/2.2/ref/settings/#login-url
+LOGIN_URL = "authnapp:login"
